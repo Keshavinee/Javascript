265cdb99053d965aeb5043e2d07c2da021da4d0e
Keshavinee <62760584+Keshavinee@users.noreply.github.com>
2022-05-21 11:56:52 +0530

    Added comment

--- Simple_and_Compound_Interest.js
+++ Simple_and_Compound_Interest.js
@@ -1,3 +1,55 @@
+/*
+ * calculateSimpleInterest calculates and returns the simple interest
+ * (floor value) for a fixed deposit. Formula used is,
+
+ * calculateSimpleInterest calculates and returns the simple interest
+ * for a fixed deposit. Formula used is,
+ * Simple Interest: P X R X T / 100
+ *   where:
+ *   P = Principal
+ *   I = Daily interest rate
+ *   N = Number of days
+ *
+ *  In case of any input error (wrong date format, alphabets in daily interest etc.), return -1
+ *
+ * @param {number} principal  - Principal amount
+ * @param {number} dailyInterest  - daily interest rate
+ * @param {string} startingDate  - Starting date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @param {string} endingDate  - Ending date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @return {number} interest
+*/
+
+/*
+ * calculateCompoundInterest calculates and returns the compound interest
+ * (floor value) for a fixed deposit. Formula used is,
+ *   Compound Interest=P[(1+I/100)^N - 1]
+ *   where:
+ *   P = Principal
+ *   I = Daily interest rate
+ *   N = Number of days
+ *
+ *  In case of any input error (wrong date format, alphabets in daily interest etc.), return -1
+ *
+ * @param {number} principal  - Principal amount
+ * @param {number} dailyInterest  - daily interest rate
+ * @param {string} startingDate  - Starting date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @param {string} endingDate  - Ending date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @return {number} interest
+*/
+
+/*
+ * extraAmountPercentage calculates and returns the extra amount percentage borrower will have to pay in case of
+ * compound interest (floor value) in comparison to the simple interest for a fixed deposit.
+
+ *  In case of any input error (wrong date format, alphabets in daily interest etc.), return -1
+ *
+ * @param {number} principal  - Principal amount
+ * @param {number} dailyInterest  - Daily interest rate.
+ * @param {string} startingDate  - Starting date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @param {string} endingDate  - Ending date of the fixed deposit in "YYYY-MM-DD" format, example "2015-03-25"
+ * @return {number} percentage
+*/
+
 function dateIsValid(dateStr) {
   const regex = /^\d{4}-\d{2}-\d{2}$/;
 
